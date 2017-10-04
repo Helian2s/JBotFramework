31662fa79f56616d0897d7cabb3de76bfdcd7d8e
helian2s <val.helian2s@gmail.com>
2017-10-04 22:18:22 +0300

    commit

--- src/main/java/com/helian2s/jbotframework/botconnectorconsumer/Application.java
+++ src/main/java/com/helian2s/jbotframework/botconnectorconsumer/Application.java
@@ -1,4 +1,10 @@
 package com.helian2s.jbotframework.botconnectorconsumer;
 
 public class Application {
+
+    public static void main(String args[]) {
+        //RestTemplate restTemplate = new RestTemplate();
+        //Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        //log.info(quote.toString());
+    }
 }
